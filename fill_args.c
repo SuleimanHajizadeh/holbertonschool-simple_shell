0beabceb6e529e2d38767389b557697cd6ea856d
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-10-09 07:22:40 +0000

    sd

--- fill_args.c
+++ fill_args.c
@@ -1,58 +1,73 @@
 #include "main.h"
+#include <string.h>
+#include <stdlib.h>
 
 /**
- * fill_args - split a line into arguments (no strtok)
- * @line: input line
- * Return: NULL-terminated array of arguments
+ * fill_args - Tokenizes a string into an array of arguments
+ *              without using strtok (C89 compatible)
+ * @line: The input command line
+ * Return: Null-terminated array of strings (arguments)
  */
 char **fill_args(char *line)
 {
     char **argv = NULL;
-    int argc = 0, i = 0, start = 0, len = 0;
+    int argc = 0;
+    int i, start, len;
+    char *arg;
 
-    if (!line)
+    if (line == NULL)
         return (NULL);
 
-    /* get length of input */
-    while (line[len])
+    len = 0;
+    while (line[len] != '\0')
         len++;
 
-    while (i <= len)
+    i = 0;
+    while (i < len)
     {
-        /* skip spaces */
-        while (i < len && (line[i] == ' ' || line[i] == '\t'))
+        /* Skip leading spaces and tabs */
+        while ((line[i] == ' ' || line[i] == '\t') && i < len)
             i++;
+
+        if (i >= len)
+            break;
 
         start = i;
 
-        /* find end of word */
-        while (i < len && line[i] != ' ' && line[i] != '\t')
+        /* Find the end of the word */
+        while (line[i] != ' ' && line[i] != '\t' && i < len)
             i++;
 
-        if (i > start) /* found a word */
+        /* Allocate memory for the argument (+1 for '\0') */
+        arg = malloc((i - start + 1) * sizeof(char));
+        if (arg == NULL)
         {
-            char *arg;
-            int j, word_len = i - start;
-
-            argv = realloc(argv, sizeof(char *) * (argc + 2));
-            if (!argv)
-                return (NULL);
-
-            arg = malloc(word_len + 1);
-            if (!arg)
-                return (NULL);
-
-            for (j = 0; j < word_len; j++)
-                arg[j] = line[start + j];
-            arg[word_len] = '\0';
-
-            argv[argc] = arg;
-            argc++;
+            perror("malloc");
+            exit(EXIT_FAILURE);
         }
-        i++;
+
+        /* Copy characters to arg */
+        {
+            int j;
+            for (j = 0; j < i - start; j++)
+                arg[j] = line[start + j];
+        }
+        arg[i - start] = '\0';
+
+        /* Reallocate argv array */
+        argv = (char **)realloc(argv, (argc + 2) * sizeof(char *));
+        if (argv == NULL)
+        {
+            perror("realloc");
+            exit(EXIT_FAILURE);
+        }
+
+        argv[argc] = arg;
+        argc++;
     }
-    if (argv)
-        argv[argc] = NULL;
+
+    if (argv != NULL)
+        argv[argc] = NULL; /* NULL-terminate argv */
 
     return (argv);
 }
